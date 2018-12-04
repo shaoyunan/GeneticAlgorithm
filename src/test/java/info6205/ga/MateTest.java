5cb37d2ce7e99c8e456174ce76b6ba0e93bbd18d
Qiao Yuchen <qiao.yu@husky.neu.edu>
2018-12-04 13:49:27 -0500

    change to distanceTest

--- src/test/java/info6205/ga/MateTest.java
+++ src/test/java/info6205/ga/MateTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import org.junit.After;
 import info6205.ga.GeneticAlgorithm;
 import info6205.ga.Individual;
+import info6205.ga.City;
 
 
 public class MateTest {
@@ -26,20 +27,33 @@ public class MateTest {
 	 @Test
 	    public void testDistance() throws Exception {
 		 Individual in = new Individual();
-		 ArrayList<City> cities; 
-		 cities.add(1, null);
-		 cities.add(2, null);
+		 ArrayList<City> cities = new ArrayList<City>();
+		 City City1 = new City();
+		 City City2 = new City();
+		 City City3 = new City();
+		 City1.setX(1);
+		 City1.setY(1);
+		 City2.setX(2);
+		 City2.setY(2);
+		 City3.setX(3);
+		 City3.setY(3);
+		 cities.add(City1);
+		 cities.add(City2);
+		 cities.add(City3);
+		 in.setCities(cities);
 	        
-	        int totalDistance = 0;
-	        for (int i = 0; i < cities.size(); i++) {
-				City start = cities.get(i);
-				City end = cities.get(0);
-				if (i + 1 < cities.size()) {
+	     double totalDistance = 0;
+	     for (int i = 0; i < cities.size(); i++) {
+			 City start = cities.get(i);
+			 City end = cities.get(0);
+			if (i + 1 < cities.size()) {
 					end = cities.get(i + 1);
 				}
-				totalDistance += start.distanceTo(end);
-	        assertEquals(totalDistance, in.totalDistance());
-	 }
+				totalDistance += start.distanceTo(end);	        
+	        	}
+	      assertEquals(totalDistance, in.totalDistance(),0.1);
+	 }   
+	 
 	 
 	 
 	 //fitness test 
